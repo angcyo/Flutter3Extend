ca7ebe50e3f7ba6e4054122c0b466bcb019ada5b
angcyo <angcyo@126.com>
2024-03-21 14:10:36 +0800

    * ref lp module

---
+++ lp_module/lib/src/pages/lp_page_mixin.dart
@@ -0,0 +1,19 @@
+part of '../../lp_module.dart';
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @since 2023/12/21
+///
+
+mixin LpScrollPageMixin on AbsScrollPage {
+  @override
+  double? getAppBarElevation(BuildContext context) {
+    return 0;
+  }
+
+  @override
+  Widget? buildAppBarFlexibleSpace(BuildContext context) {
+    final globalTheme = GlobalTheme.of(context);
+    return linearGradientWidget(listOf(globalTheme.themeWhiteColor));
+  }
+}
---
+++ lp_module/lib/src/pages/main/home/add_device_tile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter3_app/flutter3_app.dart';
+import 'package:lp_module/lp_module.dart';
+import 'package:lp_module/src/pages/main/home/add_device_bean.dart';
+
+import '../../../../assets_generated/assets.gen.dart';
+
+///
+/// @author <a href="mailto:angcyo@126.com">angcyo</a>
+/// @since 2023/12/30
+///
+/// 添加设备,设备选择tile
+class AddDeviceTile extends StatelessWidget {
+  /// 是否选中
+  final bool isSelected;
+
+  /// 设备数据
+  final AddDeviceBean? bean;
+
+  const AddDeviceTile({
+    super.key,
+    required this.bean,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final globalTheme = GlobalTheme.of(context);
+    final bean = this.bean;
+    return [
+      lpImageWidget(bean?.assetsKey, fit: BoxFit.contain),
+      bean?.name?.text().align(Alignment.topLeft),
+      isSelected
+          ? lpSvgWidget(Assets.svg.squareChecked).align(Alignment.topRight)
+          : null,
+    ].stack(alignment: Alignment.center)!.container(
+          color: isSelected
+              ? globalTheme.accentColor.withOpacity(0.2)
+              : globalTheme.itemWhiteBgColor,
+          radius: kX,
+          padding: const EdgeInsets.all(kH),
+          borderColor: isSelected
+              ? globalTheme.accentColor
+              : globalTheme.itemWhiteBgColor,
+        );
+  }
+}
