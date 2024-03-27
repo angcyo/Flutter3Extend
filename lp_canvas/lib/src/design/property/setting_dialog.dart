52b7571558f4d991543d584c17ace1ac9ee3d38d
angcyo <angcyo@126.com>
2024-03-27 22:59:21 +0800

    + setting dialog

--- lp_canvas/lib/src/design/property/setting_dialog.dart
+++ lp_canvas/lib/src/design/property/setting_dialog.dart
@@ -7,7 +7,7 @@ import 'package:flutter3_canvas/flutter3_canvas.dart';
 /// @date 2024/03/27
 ///
 /// 画布设置对话框
-class SettingDialog extends StatelessWidget {
+class SettingDialog extends StatefulWidget {
   /// 核心对象
   final CanvasDelegate? canvasDelegate;
 
@@ -21,37 +21,67 @@ class SettingDialog extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final selectedElementCount =
-        canvasDelegate?.canvasElementManager.selectedElementCount ?? 0;
-    final elementSelectComponent =
-        canvasDelegate?.canvasElementManager.elementSelectComponent;
-    return [
-      IconTextTile(
-        iconWidget: lpCanvasSvgWidget(Assets.svg.setting),
-        text: "设置",
-        enable: selectedElementCount == 1,
-        onTap: () {
-          canvasDelegate?.canvasElementManager
-              .showPropertyLayoutWidget(elementSelectComponent);
-          checkClose(context);
-        },
-      ),
-      IconTextTile(
-        iconWidget: lpCanvasSvgWidget(Assets.svg.setting),
-        text: "设置画布",
-        enable: true,
-        onTap: () {
-          canvasDelegate?.canvasElementManager.showCanvasPropertyLayoutWidget();
-          checkClose(context);
-        },
-      ),
-    ].column();
-  }
+  State<SettingDialog> createState() => _SettingDialogState();
+}
 
-  void checkClose(BuildContext context) {
-    if (closeAfterTap) {
-      Navigator.of(context).pop();
-    }
+class _SettingDialogState extends State<SettingDialog>
+    with DialogMixin, TileMixin {
+  @override
+  Widget build(BuildContext context) {
+    final canvasDelegate = widget.canvasDelegate;
+    final axisUnit = canvasDelegate?.axisUnit;
+    return buildBottomDialog(context, [
+      IconTextTile(
+        text: "Dp单位",
+        rightWidget:
+            buildSwitchWidget(context, axisUnit is DpUnit, onChanged: (value) {
+          if (value) {
+            canvasDelegate?.axisUnit = const DpUnit();
+            updateState();
+          }
+        }),
+        //enable: selectedElementCount == 1,
+        onTap: () {
+          /*canvasDelegate?.canvasElementManager
+              .showPropertyLayoutWidget(elementSelectComponent);*/
+          closeDialogIf(context, widget.closeAfterTap);
+        },
+      ),
+      IconTextTile(
+        text: "厘米单位",
+        rightWidget:
+            buildSwitchWidget(context, axisUnit is MmUnit, onChanged: (value) {
+          if (value) {
+            canvasDelegate?.axisUnit = const MmUnit();
+            updateState();
+          }
+        }),
+        onTap: () {
+          /*canvasDelegate?.canvasElementManager.showCanvasPropertyLayoutWidget();*/
+          closeDialogIf(context, widget.closeAfterTap);
+        },
+      ),
+      IconTextTile(
+        text: "英寸单位",
+        rightWidget: buildSwitchWidget(context, axisUnit is InchUnit,
+            onChanged: (value) {
+          if (value) {
+            canvasDelegate?.axisUnit = const InchUnit();
+            updateState();
+          }
+        }),
+        onTap: () {
+          /*canvasDelegate?.canvasElementManager.showCanvasPropertyLayoutWidget();*/
+          closeDialogIf(context, widget.closeAfterTap);
+        },
+      ),
+      IconTextTile(
+        text: "test",
+        onTap: () {
+          /*canvasDelegate?.canvasElementManager.showCanvasPropertyLayoutWidget();*/
+          closeDialogIf(context, widget.closeAfterTap);
+        },
+      ),
+    ]);
   }
 }
